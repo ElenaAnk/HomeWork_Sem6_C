2d29bd917f0a17ba24ea623f6cac160e3b1f755e
Elena <elena_nn@list.ru>
2023-06-29 20:40:55 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Доп.Задача Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1. (Решить можно, если хотите потренироваться)
+//(Следующее число складывается из двух предыдущих)
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine()!);
+
+int[] Fib(int N)
+{
+    int[] result = new int[N];
+    int numb = 1;
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        result[i] = sum;
+        int numbnext = sum + numb;
+        numb = sum;
+        sum = numbnext;
+    }
+    return result;
+}
+
+int[] Fibon = Fib(N);
+Console.WriteLine($"{String.Join(" ", Fibon)}");
